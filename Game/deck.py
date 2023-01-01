60737d1c72baf973dfc84890b7c954032c15f725
David Legarre Saavedra <davidls920@gmail.com>
2023-01-01 22:56:52 +0100

    Deck generation is working in gameplay

--- Game/deck.py
+++ Game/deck.py
@@ -72,16 +72,79 @@ CARD_TO_SCORE = {
     "A" : 14, 
 }
 
-class Deck:
-    def __init__(self, DECK):
-        self.deck = self.shuffle(DECK)
+# The ranks and suits of the cards
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
 
-    def shuffle(deck):
+class Card: 
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.rank} of {self.suit}"
+
+
+class Hand:
+    def __init__(self, cards) -> None:
+        self.cards = cards
+
+    def add_card(self,card):
+        self.cards.append(card)
+    
+    def remove_card(self, card):
+        try:
+            self.cards.remove(card)    
+        except:
+            print(f"This hand does not have {card}")
+
+    def __repr__(self):
+        return f"{', '.join(str(card) for card in self.cards)}"
+
+
+class Player:
+    def __init__(self, money):
+        self.money = money
+        self.hand = Hand([])
+
+
+class Deck:
+    def __init__(self):
+        self.deck = self.generate_deck()
+        self.deck = self.shuffle(self.deck)
+
+    def generate_deck(self):
+        deck = []
+        for suit in suits:
+            for rank in ranks:
+                deck.append(Card(rank,suit))
+        return deck
+
+    def shuffle(self, deck):
         # Use the Fisher-Yates shuffle algorithm to shuffle the deck
         for i in range(len(deck)-1, 0, -1):
             # Generate a random index j such that 0 <= j <= i
-            j = random.int(0, i)
+            j = random.randint(0, i)
             # Swap the cards at indices i and j
             deck[i], deck[j] = deck[j], deck[i]
 
         return deck
+
+    def deal_card(self, player):
+        try:
+            player.hand.add_card(self.deck.pop())
+        except:
+            print("Deck is empty")
+
+    def return_card(self, card):
+        self.deck.append(card)
+
+    def deal_hand(self,player):
+        for i in range(5):
+            self.deal_card(player)
+
+    def __repr__(self):
+        return f"{', '.join(str(card) for card in self.deck)}"
+
+    def __len__(self):
+        return len(self.deck)
--- Game/gameplay.py
+++ Game/gameplay.py
@@ -1,5 +1,19 @@
+from deck import *
+
 class Gameplay:
     def __init__(self, num_players, deck):
         self.num_players = num_players
         self.deck = deck
         self.players = []
+
+
+
+
+def main():
+    game = Gameplay(2,Deck())
+    print(game.deck)
+    print(len(game.deck))
+
+
+if __name__=='__main__':
+    main()
