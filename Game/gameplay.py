c18217f102e51b79ccafb68fb30d4d611f22df0c
DavidLegarre <davidls920@gmail.com>
2023-01-29 21:16:09 +0100

    Deleted old implementations and added turn-based control

--- Game/gameplay.py
+++ Game/gameplay.py
@@ -1,12 +1,20 @@
 from enum import Enum
 from collections import Counter
+import os
+
 from cards import Deck, compare_plays
 from player import Player
 
 MONEY = 1000
+MAX_TURNS = 50
 NUM_PLAYERS = 2
 # Define the base bet to start a game
 MIN_BET = 50
+
+
+def clear():
+    # Clear the console
+    os.system('cls')
 
 
 class Gameplay:
@@ -71,47 +79,69 @@ class Gameplay:
 
     def play(self):
         current_bid = MIN_BET
-        playing = True
         ended_turns = 0
 
-        self.deck.shuffle()
+        # self.deck.shuffle()
+        clear()
 
-        while playing:
-            for i, player in enumerate(self.players):
-                i += 1
-                current_bid, player_action = self.turn(i, player, current_bid)
+        for i, player in enumerate(self.players):
+            i += 1
+            current_bid, player_action = self.turn(i, player, current_bid)
 
-                # If any player folds the other one wins
-                if player_action is self.Actions.fold.value:
-                    winner = [player for player in self.players
-                              if not player._folded][0]
-                    self.wins(winner, current_bid)
-                    ended_turns += 1
-                    print(winner)
-                    playing = False
-                    break
+            # If any player folds the other one wins
+            if player_action is self.Actions.fold.value:
+                winner = [player for player in self.players
+                          if not player._folded][0]
+                self.wins(winner, current_bid)
+                ended_turns += 1
+                print(winner)
+                break
 
-                # Avoid skipping turn when player 1 decides to check
-                if ((i == 1) and (player_action is self.Actions.check.value)):
-                    continue
+            # Avoid skipping turn when player 1 decides to check
+            if ((i == 1) and (player_action is self.Actions.check.value)):
+                continue
 
-                bets = list(
-                    Counter(player._bet for player in self.players).values())
+            bets = list(
+                Counter(player._bet for player in self.players).values())
 
-                if bets[0] == len(self.players):
-                    # For now only 2 players are allowed
-                    winner = compare_plays(self.players[0], self.players[1])
-                    winner_i = self.players.index(winner)
-                    print(f"Player {winner_i+1} has won the round")
-                    self.wins(winner, current_bid)
-                    playing = False
+            if bets[0] == len(self.players):
+                # For now only 2 players are allowed
+                winner = compare_plays(self.players[0], self.players[1])
+                winner_i = self.players.index(winner)
+                print(f"Player {winner_i+1} has won the round")
+                self.wins(winner, current_bid)
+        print("New Game? Y/N")
+        option = input("")
+        while True:
+            match option.lower():
+                case "y":
+                    return True
+                case "n":
+                    print("Well played!")
+                    print("Goodbye!")
+                    return False
+                case _:
+                    print("Wrong input try again (Y/N):")
+                    option = input("")
 
-        return 0
+    def isKeepPlaying(self):
+        for player in self.players:
+            if player._money <= 0:
+                return False
+        return True
 
 
 def main():
     game = Gameplay(NUM_PLAYERS)
-    game.play()
+    turns = 0
+    playing = True
+    while (
+        (turns < MAX_TURNS or
+         not game.isKeepPlaying()) and
+        playing
+    ):
+        playing = game.play()
+        turns += 1
 
 
 if __name__ == '__main__':
